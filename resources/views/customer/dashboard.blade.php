ead3a7db422f272e924eb1597128fb54e1007868
paullarasales <ahmadzufukli@gmail.com>
2024-08-29 23:06:13 +0800

    removed the filter logic

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -66,57 +66,7 @@
             const filterSelect = document.getElementById('filter');
             const productList = document.getElementById('product-list');
 
-            filterSelect.addEventListener('change', function() {
-                const selectedFilter = this.value;
-                
-                fetch(`{{ route('user.productFilter' )}}?filter=${selectedFilter}`, {
-                    method: 'GET',
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'Accept': 'application/json',
-                    },
-                })
-                .then(response => response.json())
-                .then(data => {
-                    updateProductList(data.products);
-                });
-                .catch(error => console.error('Error:', error));
-            });
-
-            function updateProductList(products) {
-                productList.innerHTML = '';
-
-                if (products.length === 0) {
-                    productList.innetHTML = '<div class="col-span-full text-center text-gray-600">No Products To Display</div>'
-                } else {
-                    products.forEach(product => {
-                        const productElement = createProductElement(product);
-                        productList.appendChild(productElement);
-                    });
-                }
-            }
-
-            function createProductElement(product) {
-            const div = document.createElement('div');
-            div.className = 'bg-white rounded-lg shadow-lg overflow-hidden';
-            div.innerHTML = `
-                <img src="{{ asset($product->photo) }}" alt="Product Image" class="w-full h-32 md:h-28 object-cover sm:h-32">
-                    <div class="p-4 flex flex-col justify-between flex-grow">
-                        <div class="mb-4">
-                            <h3 class="text-lg font-semibold text-gray-800">{{ $product->product_name }}</h3>
-                                <p class="text-sm text-gray-600">Price: ₱{{ $product->price }}</p>
-                                <p class="text-sm text-gray-600">Description: {{ $product->description }}</p>
-                        </div>
-                            <form action="{{ route('add-to-cart', ['productId' => $product->id]) }}" method="POST" class="mt-auto">
-                                @csrf
-                                <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                <button type="submit" class="bg-yellow-500 text-white px-4 py-2 rounded-md">Add to Cart</button>
-                            </form>
-                        </div>
-            `;
-            return div;
-        }
-
+            
             if (notificationBanner) {
                 setTimeout(() => {
                     notificationBanner.style.opacity = '0';
